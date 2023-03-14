b5382a956e1b492a60cd7456090981403b7e60d3
Joaquin Arias <joaquinarias989@gmail.com>
2023-03-14 14:13:02 -0300

    feat: add About section and refact proyect

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#292929',
+      },
+      //   borderWidth: {
+      //     2: '3px',
+      //   },
+    },
+  },
+  plugins: [],
+};
